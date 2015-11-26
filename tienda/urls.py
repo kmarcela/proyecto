15c34b8df2aae5f0679d6e3edc9e18490cab9a3f
kmarcela <kmarcela@mesoamericana.edu.gt>
2015-11-26 10:25:49 -0600

    Modificacion

--- tienda/urls.py
+++ tienda/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
         url(r'^$', views.vertienda),
         url(r'^venta/(?P<pk>[0-9]+)/$', views.detalleproducto),
         url(r'^venta/nueva/$', views.nuevaventa, name='nuevaventa'),
+        url(r'^post/(?P<pk>[0-9]+)/editar/$', views.editar_venta, name='editar'),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import venta
 from .forms import Pubform
+from django.shortcuts import redirect
 
 def vertienda(request):
     ventas = venta.objects.all().order_by('precio')
@@ -20,4 +21,16 @@ def nuevaventa(request):
             return redirect('tienda.views.detalleproducto', pk=post.pk)
     else:
         form = Pubform()
+    return render(request, 'tienda/nuevaventa.html', {'form': form})
+
+def editar_venta(request, pk):
+    post = get_object_or_404(venta, pk=pk)
+    if request.method == "POST":
+        form = Pubform(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('tienda.views.detalleproducto', pk=post.pk)
+    else:
+        form = Pubform(instance=post)
     return render(request, 'tienda/nuevaventa.html', {'form': form})
